323c63479c661e24503a684d476083ec2dd90eb8
aredra <rnswn48@gmail.com>
2019-11-13 21:19:23 +0900

    add input tag more

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,22 +1,41 @@
 import React, {useState} from 'react';
 
 function InputSample() {
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: ''
+    });
+
+    const {name, nickname} = inputs;
 
     const onChange = (e) => {
-        setText(e.target.value);
+        const {name, value} = e.target;
+        // obj.name - 지정된 값, obj[name] - 변수 활용가능
+        // 객체 상태의 업데이트는 불변성 때문에 복사후 업데이트
+        // const nextValue = {
+        //     ...inputs
+        // }
+        // nextValue.name = value;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
     }
 
     const onReset = () => {
-        setText('');
+        setInputs({
+            name: '',
+            nickname: ''
+        });
     }
 
     return (
         <div>
-            <input onChange={onChange} value={text}/>
+            <input name='name' onChange={onChange} value={name}/>
+            <input name='nickname' onChange={onChange} value={nickname}/>
             <button onClick={onReset}>Reset Value</button>
             <div>
-                <b>Value:</b> {text}
+                <b>Value:</b> {name}({nickname})
             </div>
         </div>
     );
