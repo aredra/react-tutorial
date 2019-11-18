44113e25853b1ffd9996fd704a851b9a50efd370
aredra <rnswn48@gmail.com>
2019-11-19 00:14:35 +0900

    add class component

--- src/Hello.js
+++ src/Hello.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Hello(props) {
-    // 값이 변할 경우 삼항연사자 show/hide 일 경우 && 연산자
-    return <div style={{color: props.color}}>{props.isSpecial && <b>#</b>}Hi, Welcome Aredra!<br/>Welcome {props.name}</div>;
+class Hello extends Component {
+    static defaultProps = {
+        name: "no name",
+        color: "aqua"
+    }
+
+    render() {
+        const {color, name, isSpecial} = this.props;
+        return (
+            <div style={{color}}>
+                {isSpecial && <b>#</b>}
+                Hi, Welcome Aredra!<br/>Welcome {name}
+            </div>
+        );
+    }
 }
 
-Hello.defaultProps = {
-    name: "no name",
-    color: "aqua"
-}
+// function Hello(props) {
+//     // 값이 변할 경우 삼항연사자 show/hide 일 경우 && 연산자
+//     return <div style={{color: props.color}}>{props.isSpecial && <b>#</b>}Hi, Welcome Aredra!<br/>Welcome {props.name}</div>;
+// }
+
+// Hello.defaultProps = {
+//     name: "no name",
+//     color: "aqua"
+// }
 
 export default Hello;
--- src/counter.js
+++ src/counter.js
@@ -1,44 +1,87 @@
-import React, {useReducer} from 'react';
+import React, {Component} from 'react';
 
-function reducer(state, action) {
-    switch (action.type) {
-        case 'INCREMENT' :
-            return state + 1;
-        case 'DECREMENT' :
-            return state - 1;
-        default :
-            throw new Error('Unhandled action') ;
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0,
+            fixed: 1,
+        }
+        // this.handledDecrease = this.handledDecrease.bind(this);
+        // this.handledIncrease = this.handledIncrease.bind({foo: 'var'});
     }
-}
 
-function Counter() {
-    // const numberState = useState(0);
-    // const number = numberState[0];
-    // const setNumber = numberState[1];
-    // const [number, setNumber] = useState(0);
-    const [number, dispatch] = useReducer(reducer, 0);
-
-    const onIncrease = () => {
-        // setNumber(number + 1);
-        dispatch({
-            type: 'INCREMENT'
+    handledIncrease = () => {
+        console.log(this);
+        // this.setState({
+        //     counter: this.state.counter + 1
+        // });
+        this.setState(state => ({
+            counter: state.counter + 1
+        }));
+        this.setState(state => ({
+            counter: state.counter + 1
+        }));        
+        this.setState(state => ({
+            counter: state.counter + 1
+        }));
+    }
+    handledDecrease =  () => {
+        this.setState({
+            counter: this.state.counter - 1
         });
     }
 
-    const onDecrease = function () {
-        // setNumber(prevNumber => prevNumber - 1);
-        dispatch({
-            type: 'DECREMENT'
-        });
+    render() {
+        return (
+            <div>
+                <h2>{this.state.counter}</h2>
+                <button onClick={this.handledIncrease}>+1</button>
+                <button onClick={this.handledDecrease}>-1</button>
+            </div>  
+        )
     }
-
-    return (
-        <div>
-            <h2>{number}</h2>
-            <button onClick={onIncrease}>+1</button>
-            <button onClick={onDecrease}>-1</button>
-        </div>
-    );
 }
+
+// function reducer(state, action) {
+//     switch (action.type) {
+//         case 'INCREMENT' :
+//             return state + 1;
+//         case 'DECREMENT' :
+//             return state - 1;
+//         default :
+//             throw new Error('Unhandled action') ;
+//     }
+// }
+
+// function Counter() {
+//     // const numberState = useState(0);
+//     // const number = numberState[0];
+//     // const setNumber = numberState[1];
+//     // const [number, setNumber] = useState(0);
+//     const [number, dispatch] = useReducer(reducer, 0);
+
+//     const onIncrease = () => {
+//         // setNumber(number + 1);
+//         dispatch({
+//             type: 'INCREMENT'
+//         });
+//     }
+
+//     const onDecrease = function () {
+//         // setNumber(prevNumber => prevNumber - 1);
+//         dispatch({
+//             type: 'DECREMENT'
+//         });
+//     }
+
+//     return (
+//         <div>
+//             <h2>{number}</h2>
+//             <button onClick={onIncrease}>+1</button>
+//             <button onClick={onDecrease}>-1</button>
+//         </div>
+//     );
+// }
 
 export default Counter
