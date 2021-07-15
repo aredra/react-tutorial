1371bd87dea4381758e63a08e6ccfb67ffb3ae46
jyseo <rnswn48@gmail.com>
2021-07-15 16:28:43 +0900

    Add styled-components library sample

---
+++ styled-components-example/src/App.js
@@ -0,0 +1,56 @@
+import logo from "./logo.svg";
+import "./App.css";
+import StyledButton from "./components/StyledButton";
+import styled, { createGlobalStyle } from "styled-components";
+import StyledA from "./components/StyledA";
+
+const PrimaryStyledButton = styled(StyledButton)`
+  background: darkgreen;
+  border: none;
+  color: #fff;
+`;
+
+const UppercaseButton = (props) => (
+  <button {...props} children={props.children.toUpperCase()} />
+);
+const MyButton = (props) => (
+  <button
+    className={props.className}
+    {...props}
+    children={`MyButton ${props.children}`}
+  />
+);
+const StyledMyButton = styled(MyButton)`
+  background: darkgreen;
+  border: 2px solid ${(props) => props.color || "#fff"};
+  color: #fff;
+`;
+const GlobalStyle = createGlobalStyle`
+button {
+  color: #fff !important;
+}
+`;
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <StyledButton>Button</StyledButton>
+          <StyledButton primary>Button</StyledButton>
+          <PrimaryStyledButton>Button</PrimaryStyledButton>
+          <StyledButton as="a" href="/">
+            Button
+          </StyledButton>
+          <StyledButton as={UppercaseButton}>Button</StyledButton>
+          <StyledMyButton color="yellow" />
+          <StyledA href="https://google.com">Go Google</StyledA>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ styled-components-example/src/components/StyledButton.jsx
@@ -0,0 +1,20 @@
+import styled, { css } from "styled-components";
+
+const StyledButton = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid violet;
+  color: violet;
+  margin: 0 1em;
+  padding 0.25em 1em;
+  font-size: 20px;
+
+  ${(props) =>
+    props.primary &&
+    css`
+      background: violet;
+      color: white;
+    `}
+`;
+
+export default StyledButton;
